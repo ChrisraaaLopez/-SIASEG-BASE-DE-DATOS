e4b2f3df1f9ca3114ca4be4ff0fe095f8e010758
ChrisraaaLopez <chrisraaalopez14@gmail.com>
2025-09-26 18:21:37 -0600

    Add files via upload

---
+++ BD SIASEG/Tablas/Datos_Prueba.sql
@@ -0,0 +1,5 @@
+INSERT INTO roles (nombre_rol, descripcion)
+VALUES 
+('Administrador', 'Usuario con todos los permisos del sistema, puede gestionar empleados, roles, estaciones y turnos.'),
+('Supervisor', 'Usuario encargado de supervisar y validar las asistencias de los empleados de su área.'),
+('Operador', 'Empleado que realiza tareas operativas, su acceso se limita a registrar asistencias y ver su información.');
